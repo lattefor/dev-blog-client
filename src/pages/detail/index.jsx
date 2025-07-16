5941ca5e51de5659f6e29b0ea5f9d6cd7e1bdd29
mitsuko yoneyama <latteforever@gmail.com>
2025-07-16 14:47:34 -0700

    some styling work

---
+++ src/pages/detail/index.jsx
@@ -0,0 +1,10 @@
+import classes from "./styles.module.css";
+
+export default function Detail() {
+    return (
+        <div className={classes.wrapper}>
+            <h1>Detail</h1>
+            <p>Detail goes here....</p>
+        </div>
+    )           
+}
